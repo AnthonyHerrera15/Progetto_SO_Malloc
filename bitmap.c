3c1a019e16413f755901d52a5d97d94bc730346a
AnthonyHerrera <aliagaherrera.2005993@studenti.uniroma1.it>
2025-04-13 20:40:46 +0200

    bozza bitmap.c con piccole correzioni in bitmap.h

---
+++ bitmap.c
@@ -0,0 +1,53 @@
+#include "bitmap.h"
+
+int bitmap_getBytes(int n_bits) {
+    return (n_bits/8) + ((n_bits % 8)!=0);
+}
+
+void bitmap_init(Bitmap* bitmap, int n_bits, char* buffer) {
+    bitmap->bits = buffer;
+    bitmap->n_bits = n_bits;
+    for(int i=0; i<bitmap_getBytes(n_bits); i++) { //inizializzo tutti i bit a 0
+        bitmap->bits[i] = 0;
+    }
+}
+
+void bitmap_set(Bitmap* bitmap, int bit_num, int value){
+    if(bit_num<0 || bit_num >= bitmap->n_bits) {    //controllo che il bit_num sia valido
+        printf("ERRORE: indice del bit non valido,fuori dal bitmap\n");
+        return;  
+    }
+    int byte_index = bit_num/8;     //indice del byte dentro il buffer
+    int bit_index = bit_num%8;      //indice del bit dentro il byte
+    if(value==1) {
+        bitmap->bits[byte_index] |= (1 << bit_index);   //imposto il bit richiesto a 1
+    } else {
+        bitmap->bits[byte_index] &= ~(1 << bit_index);  //imposto il bit richiesto a 0
+    }
+}
+
+int bitmap_get(Bitmap* bitmap, int bit_num){
+    if(bit_num<0 || bit_num >= bitmap->n_bits) {    //controllo che il bit_num sia valido
+        printf("ERRORE: indice del bit non valido,fuori dal bitmap\n");
+        return -1;  
+    }
+    int byte_index = bit_num/8;     //indice del byte dentro il buffer
+    int bit_index = bit_num%8;      //indice del bit dentro il byte
+    int ris = (bitmap->bits[byte_index]) & (1 << bit_index)!=0; //restituisco il valore del bit richiesto
+    return ris;
+}
+
+void bitmap_print(Bitmap* bitmap) {
+    if(!bitmap || !bitmap->bits) {
+        printf("Bitmap non inizializzato\n");
+        return;
+    }
+    printf("Bitmap: ");
+    for(int i=0; i<bitmap->n_bits; i++) {
+        if(i % 8 == 0 && i != 0) {
+            printf(" ");
+        }
+        printf("%d", bitmap_get(bitmap, i));
+    }
+    printf("\n");
+}
--- bitmap.h
+++ bitmap.h
@@ -6,14 +6,17 @@ typedef struct{
     int n_bits;
 } Bitmap;
 
-// funzione che inizializza un bitmap 
-void bitmap_init(Bitmap* bitmap, int n_bits, char* buffer);
-
 // funzione che calcola e restituisce il numero di byte necessari per memorizzare i bits
 int bitmap_getBytes(int n_bits);
+
+// funzione che inizializza un bitmap 
+void bitmap_init(Bitmap* bitmap, int n_bits, char* buffer);
 
 // funzione che imposta un bit specifico nella bitmap ad un valore: 0 o 1. 
 void bitmap_set(Bitmap* bitmap, int bit_num, int value);
 
 // funzione che restituisce il valore di un bit specifico nella bitmap (0 o 1)
-int bitmap_getBit(Bitmap* bitmap, int bit_num);
+int bitmap_get(Bitmap* bitmap, int bit_num);
+
+// funzione che stampa il bitmap
+void bitmap_print(Bitmap* bitmap);
